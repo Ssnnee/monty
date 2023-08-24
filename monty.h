512da0bc5aef1630127209b2f60d168604548c23
Samuel Nandi <nandisamuelsne@gmail.com>
2023-08-24 12:45:26 -0700

    Edit pall prototype

--- monty.h
+++ monty.h
@@ -43,7 +43,7 @@ typedef struct instruction_s
 extern stack_t *global_stack;
 
 void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number __attribute_maybe_unused__);
+void pall(stack_t **stack, unsigned int line_number __attribute__((unused)));
 void parse_file(char *filepath);
 
 #endif
