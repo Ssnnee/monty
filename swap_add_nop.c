a9e64a4d272523fbfc97aa6a2f3d799cf288a4d5
Samuel Nandi <nandisamuelsne@gmail.com>
2023-08-24 14:36:50 -0700

    Remove the strrd params

--- swap_add_nop.c
+++ swap_add_nop.c
@@ -37,7 +37,7 @@ void add(stack_t **stack, unsigned int line_number)
 
 	if (temp == NULL || temp->next == NULL)
 	{
-		printf(stderr, "L%d: can't add, stack too short\n", line_number);
+		printf("L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
