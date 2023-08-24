97ffaef3c792f7aaed761c70c31d8f0309999f1d
Samuel Nandi <nandisamuelsne@gmail.com>
2023-08-24 14:32:37 -0700

    First two added

--- swap_add_nop.c
+++ swap_add_nop.c
@@ -22,3 +22,26 @@ void swap(stack_t **stack, unsigned int line_number)
 	temp->n = temp->next->n;
 	temp->next->n = last_value;
 }
+
+
+/**
+ * add -  Adds the top two elements of the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number of the instruction
+ *
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (temp == NULL || temp->next == NULL)
+	{
+		printf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp->next->n += temp->n;
+	*stack = temp->next;
+	free(temp);
+}
